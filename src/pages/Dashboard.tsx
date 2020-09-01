919d3083a4f58372f5433833822f530f70fbc7f9
victor-mwania <victormwnza@gmail.com>
2020-09-01 15:50:52 +0300

    project setup

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Dashboard () {
+    return(
+        <div> Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo eveniet soluta corrupti tenetur nostrum amet accusamus vel, dolorem hic, facere incidunt excepturi quasi sequi aut error vero. Saepe, rem consectetur?</div>
+    )
+}
+
+export default Dashboard;
+
