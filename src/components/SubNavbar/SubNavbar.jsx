fc1b1a648001125ec44bd9db6a211b908ccfdb84
victor-mwania <victormwnza@gmail.com>
2020-09-19 19:48:58 +0300

    create subnavbar

---
+++ src/components/SubNavbar/SubNavbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import  './SubNavbar.scss'
+
+function SubNavbar () {
+    return(
+        <div className="sub_navbar">
+            <p>Practice</p>
+            <h2>Dashboard</h2>
+        </div>
+    )
+}
+
+export default SubNavbar;
