6330f2af466b1cba8645cfa445f7716d243e79ad
victor-mwania <victormwnza@gmail.com>
2020-09-19 19:49:08 +0300

    add subnavabar

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
+import SubNavbar from '../components/SubNavbar/SubNavbar';
 
 function Dashboard () {
     return(
         <div>
             <Navbar/>
+            <SubNavbar/>
         </div>
     )
 }
