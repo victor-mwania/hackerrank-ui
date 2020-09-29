0a05c292d88deb0793358a8d974b8bc86ead3515
victor-mwania <victormwnza@gmail.com>
2020-09-29 11:10:42 +0300

    create skills card

---
+++ src/components/Skills/SkillCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {ReactComponent as ProblemSolvingIcon} from '../../assets/icons/ProblemSolving.svg';
+
+function SkillCard(props) {
+
+    const {title1, title2} = props
+        return(
+        <React.Fragment>
+            <div className="skill_card">
+                <div className="skill_card_info">
+                    <div className="title">
+                        <h3>{title1}</h3>
+                        <h3>{title2}</h3>
+                    </div>
+                    <div className="card_button">
+                        <button>Get Certified</button>
+                    </div>
+                </div>
+                <div className="icon">
+                    <ProblemSolvingIcon height="150px" width="150px" />
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default SkillCard
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-
-import './Skills.scss'
-import {ReactComponent as ProblemSolvingIcon} from '../../assets/icons/ProblemSolving.svg'
+import './Skills.scss';
+import SkillCard from './SkillCard';
 
 function Skills () {
     return(
@@ -9,25 +8,18 @@ function Skills () {
             <div className="skills">
                 <div className="skills_header">
                     <h4>Get Your Skills Certified</h4>
-                </div>
-                <div className="skills_list">
-                    <div className="skill_card">
-                        <div className="skill_card_info">
-                            <div className="title">
-                                <h3>Problem Solving</h3>
-                                <h3>(Basic)</h3>
-                            </div>
-                            <div className="card_button">
-                                <button>Get Certified</button>
-                            </div>
-                        </div>
-                        <div className="icon">
-                            <ProblemSolvingIcon
-                            height="150px"
-                            width="150px"
-                            />
-                        </div>
+                    <div className="skills_list">
+                    <SkillCard title1={"Problem Solving"} title2={"(Basic)" }/>
+                    <SkillCard title1={"Problem Solving"} title2={"(Basic)" }/>
+
+                    <SkillCard title1={"Problem Solving"} title2={"(Basic)" }/>
+
+                    <SkillCard title1={"Problem Solving"} title2={"(Basic)" }/>
+
+                    <SkillCard title1={"Problem Solving"} title2={"(Basic)" }/>
                     </div>
+                    
+
                 </div>
             </div>
         </React.Fragment>
